222377a5acb323b1de7a50b86892d6872a1bf35e
Port5k <96888438+Port5k@users.noreply.github.com>
2022-05-28 23:29:31 +0100

    Add files via upload

---
+++ class.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = []
+        self.score = score
+
+    def change_name(self, name):
+        self.name = name
+
+    def change_age(self, age):
+        self.age = age
+
+    def add_track(self, track):
+        self.tracks.append(track)
+
+    def get_score(self):
+        print(self.score)
+
+    
+        
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
